79b43be15846b7d82826a19ee885298ba2642250
Bolun Thompson <me@bolun.dev>
2024-10-01 10:19:17 -0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "CS 31";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+          name = "CS 31 Shell";
+          nativeBuildInputs = with pkgs; with llvmPackages_19; [
+            clang
+          ];
+          packages = with pkgs; with llvmPackages_19; [
+            gdb
+            binutils
+            git
+            clang-tools
+          ];
+        };
+    };
+}
